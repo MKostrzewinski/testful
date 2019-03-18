8896c2f5513f85df23f4edf237d2c5de8eeb480d
MikoTheAble <43997963+MikoTheAble@users.noreply.github.com>
2019-03-18 23:00:08 +0100

    hmmm

--- Test/kuleczka/skrypt.js
+++ Test/kuleczka/skrypt.js
@@ -9,47 +9,44 @@ let maxX = garden.clientWidth  - ball.clientWidth;
 let maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
-  let x = event.beta;  // In degree in the range [-180,180]
-  let y = event.gamma; // In degree in the range [-90,90]
+  let x = event.beta;  
+  let y = event.gamma; 
 
   output.innerHTML  = "beta : " + x + "\n";
   output.innerHTML += "gamma: " + y + "\n";
 
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
+
   if (x >  90) { x =  90};
   if (x < -90) { x = -90};
 
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
   x += 90;
   y += 90;
 
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
-  ball.style.top  = (maxX*x/90 - 1) + "px";
-  ball.style.left = (maxY*y/90 - 1) + "px";
+  ball.style.top  = (maxX*x/90 - 10) + "px";
+  ball.style.left = (maxY*y/90 - 10) + "px";
   
    if(ball.style.top < 10) {
         ball.style.top = 10;
+        koniec()
     }
     if(ball.style.bottom < 10) {
         ball.style.bottom = 10;
+        koniec()
     }
     if(ball.style.right < 10) {
         ball.style.right = 10;
+        koniec()
     }
     if(ball.style.left < 10) {
         ball.style.left = 10;
+        koniec()
     }
-    let ballPosition = ball.getBoundingClientRect();
-    let holePosition = hole.getBoundingClientRect();
   
   
     function koniec(){
 
-        let ballPosition = ball.getBoundingClientRect();
-        let holePosition = hole.getBoundingClientRect();
+        let ballPosition = ball.getBoundingClientRect();// zwraca rozmiar elementu ball
+        let holePosition = hole.getBoundingClientRect();// zwraca rozmiar elementu hole
       
       
         if((ballPosition.top <= holePosition.top) && (ballPosition.bottom >= holePosition.bottom) && (ballPosition.left >= holePosition.left) &&
