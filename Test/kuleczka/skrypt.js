a5cdffb8f3e8c4b813bb27aba67d1e5d0786c50a
MikoTheAble <43997963+MikoTheAble@users.noreply.github.com>
2019-03-18 23:25:11 +0100

    asd

--- Test/kuleczka/skrypt.js
+++ Test/kuleczka/skrypt.js
@@ -9,18 +9,26 @@ let maxX = garden.clientWidth  - ball.clientWidth;
 let maxY = garden.clientHeight - ball.clientHeight;
 
 function handleOrientation(event) {
-  let x = event.beta;  
-  let y = event.gamma; 
+  let x = event.beta;  // In degree in the range [-180,180]
+  let y = event.gamma; // In degree in the range [-90,90]
 
+  output.innerHTML  = "beta : " + x + "\n";
+  output.innerHTML += "gamma: " + y + "\n";
 
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
   if (x >  90) { x =  90};
   if (x < -90) { x = -90};
 
+  // To make computation easier we shift the range of 
+  // x and y to [0,180]
   x += 90;
   y += 90;
 
-  ball.style.top  = (maxY*y/90 - 10) + "px";
-  ball.style.left = (maxX*x/90 - 10) + "px";
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top  = (maxX*x/90 - 10) + "px";
+  ball.style.left = (maxY*y/90 - 10) + "px";
   
    if(ball.style.top < 10) {
         ball.style.top = 10;
@@ -32,26 +40,21 @@ function handleOrientation(event) {
         ball.style.right = 10;
     }
     if(ball.style.left < 10) {
-        ball.style.left = 10;       
+        ball.style.left = 10;
     }
-    koniec()
-}
-function koniec(){
-
-    let ballPosition = ball.getBoundingClientRect();// zwraca rozmiar elementu ball
-    let holePosition = hole.getBoundingClientRect();// zwraca rozmiar elementu hole
-  
-  
-    if((ballPosition.top <= holePosition.top) && (ballPosition.bottom >= holePosition.bottom) && (ballPosition.left >= holePosition.left) &&
-    (ballPosition.right <= holePosition.right)) {
-        czas = Date.now() - czasStart;
-        alert("Wygrana! \n Twój czas : " + czas);
-    }
-  
 }
 
 
 
+  let ballPosition = ball.getBoundingClientRect();
+  let holePosition = hole.getBoundingClientRect();
+
+
+  if((ballPosition.top <= holePosition.top) && (ballPosition.bottom >= holePosition.bottom) && (ballPosition.left >= holePosition.left) &&
+  (ballPosition.right <= holePosition.right)) {
+      czas = Date.now() - czasStart;
+      alert("Wygrana! \n Twój czas : " + czas);
+  }
 
 
 
