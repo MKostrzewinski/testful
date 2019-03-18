845885a57f800f930615dd1ce394cd42f8b2df5e
MikoTheAble <43997963+MikoTheAble@users.noreply.github.com>
2019-03-18 23:07:24 +0100

    sdfghj

--- Test/kuleczka/skrypt.js
+++ Test/kuleczka/skrypt.js
@@ -22,42 +22,36 @@ function handleOrientation(event) {
   x += 90;
   y += 90;
 
-  ball.style.top  = (maxY*y/90 - 1) + "px";
-  ball.style.left = (maxX*x/90 - 1) + "px";
+  ball.style.top  = (maxY*y/90 - 10) + "px";
+  ball.style.left = (maxX*x/90 - 10) + "px";
   
    if(ball.style.top < 10) {
         ball.style.top = 10;
-        koniec()
     }
     if(ball.style.bottom < 10) {
         ball.style.bottom = 10;
-        koniec()
     }
     if(ball.style.right < 10) {
         ball.style.right = 10;
-        koniec()
     }
     if(ball.style.left < 10) {
-        ball.style.left = 10;
-        koniec()
+        ball.style.left = 10;       
     }
-  
-  
-    function koniec(){
-
-        let ballPosition = ball.getBoundingClientRect();// zwraca rozmiar elementu ball
-        let holePosition = hole.getBoundingClientRect();// zwraca rozmiar elementu hole
-      
-      
-        if((ballPosition.top <= holePosition.top) && (ballPosition.bottom >= holePosition.bottom) && (ballPosition.left >= holePosition.left) &&
-        (ballPosition.right <= holePosition.right)) {
-            czas = Date.now() - czasStart;
-            alert("Wygrana! \n Twój czas : " + czas);
-        }
-      
-    }
+    koniec()
 }
+function koniec(){
 
+    let ballPosition = ball.getBoundingClientRect();// zwraca rozmiar elementu ball
+    let holePosition = hole.getBoundingClientRect();// zwraca rozmiar elementu hole
+  
+  
+    if((ballPosition.top <= holePosition.top) && (ballPosition.bottom >= holePosition.bottom) && (ballPosition.left >= holePosition.left) &&
+    (ballPosition.right <= holePosition.right)) {
+        czas = Date.now() - czasStart;
+        alert("Wygrana! \n Twój czas : " + czas);
+    }
+  
+}
 
 
 
